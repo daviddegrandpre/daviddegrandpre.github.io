2a9634873fa44ae92931efc671711f8e71815ff8
daviddegrandpre <90334082+daviddegrandpre@users.noreply.github.com>
2021-10-04 14:20:05 -0400

    Create travail1.js

---
+++ travail1.js
@@ -0,0 +1,10 @@
+ <script> 
+
+var largeur = prompt ("Choisi la largeur de ta pizza!");
+alert("Tu a choisi la largeur " + largeur + "!!");
+var airDePizza =  (largeur**2)*3.14;
+alert("L'air est " + airDePizza + " pouce");
+var volumeDeBoite = (largeur+2**2)*3;
+alert("La volume de la boite est " + volumeDeBoite + " pouces carré");
+
+</script>
