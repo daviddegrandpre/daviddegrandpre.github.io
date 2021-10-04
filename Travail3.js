8b3f6474341e2364b7401abc4007cfc6e323970f
daviddegrandpre <90334082+daviddegrandpre@users.noreply.github.com>
2021-10-04 14:20:57 -0400

    Create Travail3.js

---
+++ Travail3.js
@@ -0,0 +1,26 @@
+<script>
+ var reponse =(prompt("Donne une opreration"));
+var num1 =parseInt(prompt("Nombre 1"));
+var num2 =parseInt(prompt("Nombre 2"));
+function calcul(num1,num2) {
+ var op1 = (num1+num2);
+ var op2 = (num1-num2);
+ var op3 = (num1*num2);
+ var op4 = (num1/num2);
+ var op5 = (num1%num2);
+ 
+ 
+if(reponse=="op1"){
+  console.log(op1);
+} else if (reponse=="op2") {
+  console.log(op2);
+} else if (reponse=="op3"){
+  console.log(op3);
+} else if (reponse=="op4"){
+  console.log(op4);
+} else if (reponse=="op5"){
+  console.log(op5); } }
+ 
+  console.log("La reponse est" + calcul(num1,num2));
+
+  </script
